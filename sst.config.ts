a326e4ed223669a79e79abab021d891b0645327b
Myzomi <minhjtruong@gmail.com>
2025-02-26 20:44:21 -0500

    sst setup

--- sst.config.ts
+++ sst.config.ts
@@ -7,9 +7,21 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      stage: "default",
     };
   },
   async run() {
-    new sst.aws.SvelteKit("MyWeb");
+    const vpc = new sst.aws.Vpc("MyVpc");
+    const cluster = new sst.aws.Cluster("MyCluster", { vpc });
+  
+    new sst.aws.Service("MyService", {
+      cluster,
+      loadBalancer: {
+        ports: [{ listen: "80/http" }],
+      },
+      dev: {
+        command: "node --watch index.mjs",
+      },
+    });
   },
 });
